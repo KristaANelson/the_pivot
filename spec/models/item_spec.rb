a345250d07d61d9f9abb04fae8e3b30da445e4e9
Michael Dao <michael.dao@gmail.com>
2015-01-21 08:21:30 -0700

    last ones i swear

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,48 +17,48 @@ describe Item, type: "model"  do
     expect(item).not_to be_valid
   end
 
-  it 'is valid with all three attributes' do
-    item = Item.new(title: "Something",
-                    description: "Thing",
-                    unit_price: 1)
+  it "is valid with all three attributes" do
+    item = Item.new(title:        "Something",
+                    description:  "Thing",
+                    unit_price:   1)
     expect(item).to be_valid
   end
 
   it "cannot have a blank title" do
-    item = Item.new(title: "",
-                    description: "Thing",
-                    unit_price: 1)
+    item = Item.new(title:        "",
+                    description:  "Thing",
+                    unit_price:   1)
     expect(item).not_to be_valid
   end
 
   it "cannot have a blank description" do
-    item = Item.new(title: "Thing",
-                    description: "",
-                    unit_price: 1)
+    item = Item.new(title:        "Thing",
+                    description:  "",
+                    unit_price:   1)
     expect(item).not_to be_valid
   end
 
   it "cannot have a duplicate title" do
-    Item.create(title: "Thing",
-                       description: "asd",
-                       unit_price: "1")
-    item = Item.new(title: "Thing",
-                    description: "wer",
-                    unit_price: 2)
+    Item.create(title:       "Thing",
+                description: "asd",
+                unit_price:  "1")
+    item = Item.new(title:        "Thing",
+                    description:  "wer",
+                    unit_price:   2)
     expect(item).not_to be_valid
   end
 
   it "cannot have a price of 0" do
-    item = Item.new(title: "Thing",
-                       description: "asd",
-                       unit_price: 0)
+    item = Item.new(title:        "Thing",
+                    description:  "asd",
+                    unit_price:   0)
     expect(item).not_to be_valid
   end
 
   it "cannot have a price of 1.1" do
-    item = Item.new(title: "Thing",
-                    description: "asd",
-                    unit_price: 1.1)
+    item = Item.new(title:        "Thing",
+                    description:  "asd",
+                    unit_price:   1.1)
     expect(item).not_to be_valid
   end
 end
