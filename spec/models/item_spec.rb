790a66c58b084deb10fc5b6fb69d8b9001976703
Michael Dao <michael.dao@gmail.com>
2015-01-21 08:11:05 -0700

    changed quotes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
-describe Item, type: 'model'  do
+describe Item, type: "model"  do
 
-  it 'is invalid without a title' do
+  it "is invalid without a title" do
     item = Item.new(title: nil)
     expect(item).not_to be_valid
   end
 
-  it 'is invalid without a description' do
+  it "is invalid without a description" do
     item = Item.new(description: nil)
     expect(item).not_to be_valid
   end
 
-  it 'is invalid without a price' do
+  it "is invalid without a price" do
     item = Item.new(unit_price: nil)
     expect(item).not_to be_valid
   end
@@ -24,21 +24,21 @@ describe Item, type: 'model'  do
     expect(item).to be_valid
   end
 
-  it 'cannot have a blank title' do
+  it "cannot have a blank title" do
     item = Item.new(title: "",
                     description: "Thing",
                     unit_price: 1)
     expect(item).not_to be_valid
   end
 
-  it 'cannot have a blank description' do
+  it "cannot have a blank description" do
     item = Item.new(title: "Thing",
                     description: "",
                     unit_price: 1)
     expect(item).not_to be_valid
   end
 
-  it 'cannot have a duplicate title' do
+  it "cannot have a duplicate title" do
     Item.create(title: "Thing",
                        description: "asd",
                        unit_price: "1")
@@ -48,14 +48,14 @@ describe Item, type: 'model'  do
     expect(item).not_to be_valid
   end
 
-  it 'cannot have a price of 0' do
+  it "cannot have a price of 0" do
     item = Item.new(title: "Thing",
                        description: "asd",
                        unit_price: 0)
     expect(item).not_to be_valid
   end
 
-  it 'cannot have a price of 1.1' do
+  it "cannot have a price of 1.1" do
     item = Item.new(title: "Thing",
                     description: "asd",
                     unit_price: 1.1)
