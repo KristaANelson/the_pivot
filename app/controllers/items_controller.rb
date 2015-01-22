ebc5615a5bbb68f56232c974f7476cc18acaac11
Michael Dao <michael.dao@gmail.com>
2015-01-22 13:22:10 -0700

    merged master

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,28 @@ class ItemsController < ApplicationController
     @items = Item.all
     @categories = Category.all
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    Category.find(params[:id]).
+            update_attributes(title: params[:item][:title],
+                              description: params[:item][:description],
+                              unit_price: params[:item][:unit_price],
+                              active: params[:item][:active],
+                              image_id: params[:item][:image_id])
+  end
+
+  def new
+    @item = Item.new(title: params[:title],
+                     description: params[:description],
+                     unit_price: params[:unit_price],
+                     active: params[:active],
+                     image_id: params[:item][:image_id])
+
+  end
+
+
 end
