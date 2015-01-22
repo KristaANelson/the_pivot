41bca9f1b5d3d3f3dc119e82bed15e6bf17482fe
Michael Dao <michael.dao@gmail.com>
2015-01-22 15:07:04 -0700

    hound - fixed spacing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,26 +10,26 @@ class ItemsController < ApplicationController
 
   def update
     Category.find(params[:id]).
-            update_attributes(title: params[:item][:title],
-                              description: params[:item][:description],
-                              unit_price: params[:item][:unit_price],
-                              active: params[:item][:active],
-                              image_id: params[:item][:image_id])
+            update_attributes(title:        params[:item][:title],
+                              description:  params[:item][:description],
+                              unit_price:   params[:item][:unit_price],
+                              active:       params[:item][:active],
+                              image_id:     params[:item][:image_id])
   end
 
   def new
-    @item = Item.new(title: params[:title],
-                     description: params[:description],
-                     unit_price: params[:unit_price],
-                     active: params[:active],
-                     image_id: params[:item][:image_id])
+    @item = Item.new(title:         params[:title],
+                     description:   params[:description],
+                     unit_price:    params[:unit_price],
+                     active:        params[:active],
+                     image_id:      params[:item][:image_id])
   end
 
   def create
-    Item.create(title: params[:item][:title],
-                              description: params[:item][:description],
-                              unit_price: params[:item][:unit_price],
-                              active: params[:item][:active],
-                              image_id: params[:item][:image_id])
+    Item.create(title:        params[:item][:title],
+                description:  params[:item][:description],
+                unit_price:   params[:item][:unit_price],
+                active:       params[:item][:active],
+                image_id:     params[:item][:image_id])
   end
 end
