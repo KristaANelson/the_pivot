7bb811a08ed6ee7884981b808a30b5fd35e181bb
Michael Dao <michael.dao@gmail.com>
2015-01-22 18:03:19 -0700

    added admin model

---
+++ db/migrate/20150123005925_create_admins.rb
@@ -0,0 +1,12 @@
+class CreateAdmins < ActiveRecord::Migration
+  def change
+    create_table :admins do |t|
+      t.string :full_name
+      t.string :email
+      t.string :password_digest
+      t.string :display_name
+
+      t.timestamps null: false
+    end
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
+  factory :admin do
+    fullname "Yolo Ono"
+    email "nope@nope.com"
+    password "testtest"
+    display_name "This Is Your Admin"
+  end
+
   factory :user do
     full_name "John Bob Smith"
     email "john@bobo.com"
