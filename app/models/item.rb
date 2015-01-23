19b6d8c7165a7d12fb85b66d208b07050922e557
Nathan Owsiany <nowsiany@gmail.com>
2015-01-23 00:44:14 -0700

    move dollar format calculations out of views to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper
+
   has_many :category_items
   has_many :categories, through: :category_items
   has_many :order_items
@@ -8,4 +10,8 @@ class Item < ActiveRecord::Base
   validates :description, presence: true, allow_blank: false
   validates :unit_price, presence: true, allow_blank: false,
     numericality: { only_integer: true, greater_than: 0 }
+
+  def dollar_amount
+    number_to_currency(unit_price/100)
+  end
 end
