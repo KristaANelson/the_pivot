148c4f52228dfe5876214671b696200383518515
Emily Berkeley <eberkeley@gmail.com>
2015-01-24 16:32:48 -0700

    Add create of OrderItems when user clicks checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper
+
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
@@ -7,5 +9,9 @@ class Order < ActiveRecord::Base
 
   def formatted_created_at
     created_at.strftime("%I:%M%P on %A, %B %-d, %Y")
+  end
+
+  def total_dollar_amount
+    number_to_currency(total_price / 100)
   end
 end
