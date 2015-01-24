04db4c0daded3a66fee26aaa93903ab49e40f482
Nathan Owsiany <nowsiany@gmail.com>
2015-01-23 18:37:29 -0700

    fix hound parens space issue

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.create( user_id: params[:user_id] )
+    @order = Order.create(user_id: params[:user_id])
     redirect_to order_path(@order)
   end
 end
