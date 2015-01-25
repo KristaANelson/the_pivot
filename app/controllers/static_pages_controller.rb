275c8c445ea5afa6eef13d6394ded0b1d11f0257
Michael Dao <michael.dao@gmail.com>
2015-01-25 12:05:37 -0700

    any non defined routes now get sent to the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,10 @@ class StaticPagesController < ApplicationController
 
   def admin
   end
+
+  def not_found
+    redirect_to root_path
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,5 @@ Rails.application.routes.draw do
     resources :items, only: [:index]
   end
 
-end
+  get "*rest" => "static_pages#not_found"
+ end
