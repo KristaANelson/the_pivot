264655b1f5b72426fb0f06f02fda4a203b7a9471
Emily Berkeley <eberkeley@gmail.com>
2015-01-25 15:21:51 -0700

    Add item view spec to ensure add to cart is not visible for retired items

---
+++ spec/integration/item_view_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe "the user item view" do
+  include Capybara::DSL
+  attr_reader :image
+
+  before(:each) do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController). to receive(:current_user).
+    and_return(user)
+    @image = create(:image)
+  end
+
+  it "shows an add to cart button for active items" do
+    item = create(:item, image_id: @image.id)
+
+    visit item_path(item.id)
+
+    expect(page).to have_button("Add to cart")
+  end
+
+  it "does not show an add to cart button for retired items" do
+    item = create(:item, active: false, image_id: @image.id)
+
+    visit item_path(item.id)
+
+    expect(page).to_not have_button("Add to cart")
+  end
+end
