d48b6cbb62f5b2a52b5748a322281d39eb95c20e
Michael Dao <michael.dao@gmail.com>
2015-01-24 20:38:47 -0700

    fixed hound line too long

--- spec/integration/admin_view_spec.rb
+++ spec/integration/admin_view_spec.rb
@@ -5,7 +5,9 @@ describe "the admin view", type: feature do
 
   before(:each) do
     admin = create(:admin)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).
+      and_return(admin)
     visit admin_path
   end
 
