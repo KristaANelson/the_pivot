9a63c8480507d12b1e34286acea8ac888dda2b5d
KristaANelson <Krista.A.Nelson@gmail.com>
2015-01-26 11:10:18 -0700

    Update user_views_spec.rb

--- spec/integration/user_views_spec.rb
+++ spec/integration/user_views_spec.rb
@@ -258,13 +258,13 @@ describe "the user" do
       allow_any_instance_of(ApplicationController). to receive(:current_user).
                                                     and_return(user)
       order = Order.create(user_id: user.id,
-      status:  "ordered",
-      total_price: 14678)
+        status:  "ordered",
+        total_price: 14678)
       item = create(:item)
-      order_item = OrderItem.create(order_id:        order.id,
-      item_id:         item.id,
-      quantity:        5,
-      line_item_price: 5 * item.unit_price)
+      order_item = OrderItem.create(order_id: order.id,
+        item_id: item.id,
+        quantity: 5,
+        line_item_price: 5 * item.unit_price)
       order.order_items << order_item
       visit root_path
 
