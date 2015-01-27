db1b1de37510f9b1628c6fad141bcc123cad4706
Emily Berkeley <eberkeley@gmail.com>
2015-01-27 15:29:06 -0700

    set up order view spec

---
+++ spec/integration/order_view_spec.rb
@@ -0,0 +1,100 @@
+require "spec_helper"
+
+describe "the order view" do
+  include Capybara::DSL
+  attr_reader :item, :user
+
+
+  it "shows the order total" do
+    mock_user
+    create_one_item_with_one_category
+    add_item_five_times_to_cart
+
+    visit cart_path
+    click_link_or_button("Checkout")
+
+    expect(page).to have_content("Order Summary")
+    expect(page).to have_content("Order Total: $#{5 * @item.unit_price / 100}")
+  end
+
+  it "shows the order time and status" do
+    mock_user
+    create_one_item_with_one_category
+    add_item_five_times_to_cart
+
+    visit cart_path
+    click_link_or_button("Checkout")
+
+    expect(page).to have_content("Order placed at: ")
+    expect(page).to_not have_content("Order cancelled at: ")
+    expect(page).to_not have_content("Order completed at: ")
+    expect(page).to have_content("Current status: ordered")
+  end
+
+  it "shows links for each order item" do
+    mock_user
+    create_one_item_with_one_category
+    add_item_five_times_to_cart
+
+    visit cart_path
+    click_link_or_button("Checkout")
+
+    within("table") do
+      click_link("#{@item.title}")
+    end
+
+    expect(current_path).to eq(item_path(@item.id))
+    expect(page).to have_link("#{@item.title}")
+  end
+
+  it "shows the order completed time if order completed" do
+    mock_user
+    create_one_item_with_one_category
+    add_item_five_times_to_cart
+
+    visit cart_path
+    click_link_or_button("Checkout")
+    order = user.orders.all.first
+    order.update_attributes(status: "completed")
+    visit order_path(order.id)
+
+    expect(page).to have_content("Order completed at: ")
+    expect(page).to have_content("Current status: completed")
+  end
+
+  it "shows the order cancelled time if order cancelled" do
+    mock_user
+    create_one_item_with_one_category
+    add_item_five_times_to_cart
+
+    visit cart_path
+    click_link_or_button("Checkout")
+    order = user.orders.all.first
+    order.update_attributes(status: "cancelled")
+    visit order_path(order.id)
+
+    expect(page).to have_content("Order cancelled at: ")
+    expect(page).to have_content("Current status: cancelled")
+  end
+
+  def mock_user
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController).
+    to receive(:current_user).
+    and_return(user)
+  end
+
+  def create_one_item_with_one_category
+    image = create(:image)
+    @item = create(:item, image_id: image.id)
+    category = create(:category)
+    item.categories << category
+  end
+
+  def add_item_five_times_to_cart
+    visit menu_path
+    5.times do
+      first(:button, "Add to cart").click
+    end
+  end
+end
