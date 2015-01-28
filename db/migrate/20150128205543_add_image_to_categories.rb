a3a8a92f5e37093c724f75e8b6be35be8f25960b
Nathan Owsiany <nowsiany@gmail.com>
2015-01-28 15:59:19 -0700

    removed unused migration

--- db/migrate/20150128205543_add_image_to_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddImageToCategories < ActiveRecord::Migration
-  def change
-    add_reference :categories, :image, index: true
-    add_foreign_key :categories, :images
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128205543) do
+ActiveRecord::Schema.define(version: 20150123005925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,7 @@ ActiveRecord::Schema.define(version: 20150128205543) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "image_id"
   end
-
-  add_index "categories", ["image_id"], name: "index_categories_on_image_id", using: :btree
 
   create_table "category_items", force: :cascade do |t|
     t.integer  "item_id"
@@ -99,7 +96,6 @@ ActiveRecord::Schema.define(version: 20150128205543) do
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "categories", "images"
   add_foreign_key "category_items", "categories"
   add_foreign_key "category_items", "items"
   add_foreign_key "items", "images"
