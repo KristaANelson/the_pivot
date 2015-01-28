0f8efa2e25308198f3baafeacec9d3d31da5546d
Nathan Owsiany <nowsiany@gmail.com>
2015-01-28 15:52:03 -0700

    added default image spec

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,11 @@
 class Image < ActiveRecord::Base
   has_many :items
+  has_many :categories
 
   has_attached_file :img,
     styles: { item_manage_list: "75x75>",
               menu_list: "150x150>",
-              item_list: "400x400>" },
-    default_url: "/images/:style/missing.jpg"
+              item_list: "400x400>" }
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
 
   validates :title, presence: true, uniqueness: true
