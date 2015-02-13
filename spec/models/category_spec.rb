8cd7393a95754ac3bff62b0f4b453b184c308309
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-02-12 22:03:40 -0700

    test for validation and test for invalidation without name added to category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,24 +9,11 @@ RSpec.describe Category, type: :model do
     expect(category).to be_valid
   end
 
-  xit "is not valid without a name" do
+  it "is not valid without a name" do
     category.name = nil
     expect(category).to_not be_valid
   end
 
-  xit "can have many items" do
-    category.items.create(
-    title: "Green Chili Burrito",
-    description: "Amazingly spicey burrito",
-    price: 655
-    )
-    category.items.create(
-    title: "Hot n Spicy",
-    description: "spicey burrito",
-    price: 700
-    )
-    expect(category.items.count).to eq 2
-    expect(category.items.map(&:title)).to eq(["Green Chili Burrito",
-      "Hot n Spicy"])
-    end
+  xit "can have many events" do
   end
+end
