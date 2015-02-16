8a04811844253dc1a4c03f2f08a9138f3ee1c41c
KristaANelson <krista.a.nelson@gmail.com>
2015-02-16 12:47:59 -0700

    add slug column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,25 @@ class User < ActiveRecord::Base
   has_many :orders
   has_many :order_items, through: :orders
 
+  before_save :generate_slug
+
   validates :full_name, presence: true, length: { in: 5..100 },
   format: { with: /\A[a-z ,.'-]+\z/i,  message: "Incorrect name format" }
-
   validates :email, presence: true, length: { maximum: 255 },
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Invalid" },
   uniqueness: { case_sensitive: false }
-
   validates :password, presence: true
-
   validates :password_confirmation, presence: true
-
-  validates :display_name, length: { in: 2..32 }, allow_blank: true
+  validates :display_name, length: { in: 2..32 }, presence: true
+  validates :slug, uniqueness: true
 
   def admin?
     false
+  end
+
+  private
+
+  def generate_slug
+    self.slug = display_name.parameterize
   end
 end
