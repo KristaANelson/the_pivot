89333f39245fb0ee546f0a2c507b637dc81c9ae8
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-02-15 23:26:32 -0700

    added events relationship test to venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,4 @@
 class Venue < ActiveRecord::Base
   validates :name, :location, presence: true
+  has_many :events
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -18,5 +18,10 @@ RSpec.describe Venue, type: :model do
     venue.location = nil
     expect(venue).to_not be_valid
   end
-  
+
+  it "can have many events" do
+    venue.events.create(title: "The Hobbit", description: "Hobbit movie 3")
+    expect(venue.events.map(&:venue_id)).to eq([venue.id])
+  end
+
 end
