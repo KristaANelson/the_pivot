8b3822506e0b85e8177194be0182558eff3e7a85
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-02-18 22:20:07 -0700

    using scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,13 +13,18 @@ class Event < ActiveRecord::Base
   has_many :categories, through: :categorizations
   has_many :items
 
-  def self.active(venue = nil)
-    event = where("date >= ?", Date.today)
-    .where("approved = ?", true)
-    .joins(:items).uniq
-    .where(items: { pending: false })
-    .where(items: { sold: false })
-    event = event.where("venue_id = ?", venue.id) if venue
-    event
-  end
+  scope :active_events, -> { where("venue_id = ?", Venue.id).active}
+  scope :active,      -> { joins(:items).merge(Item.available).open_events }
+  scope :open_events, -> { where("date >= ?", Date.today).is_approved }
+  scope :is_approved, -> { where approved: true }
+
+  # def self.active(venue = nil)
+  #   event = where("date >= ?", Date.today)
+  #   .where("approved = ?", true)
+  #   .joins(:items).uniq
+  #   .where(items: { pending: false })
+  #   .where(items: { sold: false })
+  #   event = event.where("venue_id = ?", venue.id) if venue
+  #   event
+  # end
 end
